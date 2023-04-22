e36c9e1deb37f0695a31ff441325732213af8d13
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2023-04-22 13:52:06 +0100

    Add initial hero heading styles

--- src/components/HeroHeading.tsx
+++ src/components/HeroHeading.tsx
@@ -1,11 +1,20 @@
 const HeroHeading = () => {
 	return (
 		<div className="hero-heading">
-			<div>Welcome to VynilVerse! v1.0.0</div>
-			<div>
-				Search your favourite music * Make list of favs * 2 x faster now!
+			<div className="hero-heading__title-wrapper">
+				<span>Welcome to</span>
+				<h1 className="hero-heading__title">VynilVerse</h1>
+				<span className="hero-heading__version-tag"> v1.0.0</span>
 			</div>
-			<div>❤️</div>
+
+			<ul className="hero-heading__subtitle">
+				<li className="hero-heading__subtitle-item">
+					Search for your favourite music
+				</li>
+				<li className="hero-heading__subtitle-item">Create your list</li>
+				<li className="hero-heading__subtitle-item">2 x faster now!</li>
+			</ul>
+			<div className="hero-heading__favourites-btn">My list ❤️</div>
 		</div>
 	);
 };
