6efe31842f95eadfc3cbde340b8b0659639ee143
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2023-04-22 15:47:34 +0100

    Add basic styling for card

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,7 +1,20 @@
 import { ItemCardInterface } from "../app/interfaces";
 
 const ItemCard = ({ title, type, image, year, format }: ItemCardInterface) => {
-	return <div className="item-card"></div>;
+	return (
+		<div className="item-card">
+			<div className="item-card__image">
+				<img src={image} alt={title} />
+			</div>
+			<div className="item-card__info">
+				<span className="item-card__title">{title}</span>
+				{year && <span className="item-card__title">release year: {year}</span>}
+				{format && (
+					<span className="item-card__title">alailable as: {format}</span>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default ItemCard;
