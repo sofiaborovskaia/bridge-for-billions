b51877d89019101db2f930f1fd70dccf8d58ff8f
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2023-04-24 15:04:42 +0100

    Improve responsive styles for hero heading

--- src/components/HeroHeading.tsx
+++ src/components/HeroHeading.tsx
@@ -2,19 +2,22 @@ const HeroHeading = () => {
 	return (
 		<div className="hero-heading">
 			<div className="hero-heading__title-wrapper">
-				<span>Welcome to</span>
+				<span className="hero-heading__welcome-text">Welcome to</span>
 				<h1 className="hero-heading__title">VynilVerse</h1>
 				<span className="hero-heading__version-tag"> v1.0.0</span>
 			</div>
 
-			<ul className="hero-heading__subtitle">
+			<ul className="hero-heading__subtitle mobile-hide">
 				<li className="hero-heading__subtitle-item">
 					Search for your favourite music
 				</li>
 				<li className="hero-heading__subtitle-item">Create your list</li>
 				<li className="hero-heading__subtitle-item">2 x faster now!</li>
 			</ul>
-			<div className="hero-heading__favourites-btn">My list ❤️</div>
+			<span className="hero-heading__subtitle desktop-hide">
+				Your ultimate music search engine
+			</span>
+			<div className="hero-heading__show-favs-button">My list ❤️</div>
 		</div>
 	);
 };
