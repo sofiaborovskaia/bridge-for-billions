8b1fd017988c53f1c489834222c6d71f5a317a75
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2023-04-25 16:50:21 +0100

    Add icons for prev and next buttons with MUI, add basic styling

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,10 +1,32 @@
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
+
 const Pagination = () => {
 	// Create a state for current page
 	// create a state for total pages
 
 	// prev - active num / total num - next
 
-	return <div className="pagination"></div>;
+	let currentPage = 1;
+	let totalPages = 16;
+
+	return (
+		<div className="pagination">
+			<button className={currentPage < 2 ? "invisible" : "pagination__button"}>
+				<ArrowBackRoundedIcon />
+			</button>
+			<span className="pagination__number">
+				{currentPage}/{totalPages}
+			</span>
+			<button
+				className={
+					currentPage === totalPages ? "invisible" : "pagination__button"
+				}
+			>
+				<ArrowForwardRoundedIcon />
+			</button>
+		</div>
+	);
 };
 
 export default Pagination;
